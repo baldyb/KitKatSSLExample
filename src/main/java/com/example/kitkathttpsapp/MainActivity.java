263e9d8ba03e3a28ae74496e9ddbddbc596af7ea
martin <mart.barber@gmail.com>
2019-05-06 22:38:49 +0100

    added rest of files

---
+++ src/main/java/com/example/kitkathttpsapp/MainActivity.java
@@ -0,0 +1,157 @@
+package com.example.kitkathttpsapp;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpVersion;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ClientConnectionManager;
+import org.apache.http.conn.scheme.PlainSocketFactory;
+import org.apache.http.conn.scheme.Scheme;
+import org.apache.http.conn.scheme.SchemeRegistry;
+import org.apache.http.conn.ssl.SSLSocketFactory;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpParams;
+import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.HTTP;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import static java.security.AccessController.getContext;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        try {
+            // perform long lived async task ... read something from URL/API etc
+            BackgroundTask bt = new BackgroundTask(MainActivity.this);
+            bt.execute();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+        Log.d("Test", "onCreate: ");
+    }
+
+
+    private class BackgroundTask extends AsyncTask<Void, Void, Void> {
+        private ProgressDialog dialog;
+
+        MainActivity parent = null;
+
+        public BackgroundTask(MainActivity activity) {
+            parent = activity;
+            dialog = new ProgressDialog(activity);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            //dialog.setMessage("Doing something, please wait.");
+            dialog.show();
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            if (dialog.isShowing()) {
+                dialog.dismiss();
+            }
+
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                doSSLRequest("https://slashdot.org/");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+            return null;
+        }
+
+    }
+
+
+    //
+    public static String doSSLRequest(String URL) {
+        DefaultHttpClient httpClient = null;
+
+        try {
+
+
+
+            // Setup a custom SSL Factory object which simply ignore the certificates validation and accept all type of self signed certificates
+       //     SSLSocketFactory sslFactory = new SimpleSSLSocketFactory(null);
+         //   sslFactory.setHostnameVerifier(SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+
+            // Enable HTTP parameters
+            HttpParams params = new BasicHttpParams();
+            HttpProtocolParams.setVersion(params, HttpVersion.HTTP_1_1);
+            HttpProtocolParams.setContentCharset(params, HTTP.UTF_8);
+
+            /*
+            // Register the HTTP and HTTPS Protocols. For HTTPS, register our custom SSL Factory object.
+            SchemeRegistry registry = new SchemeRegistry();
+            registry.register(new Scheme("http", PlainSocketFactory.getSocketFactory(), 80));
+            registry.register(new Scheme("https", sslFactory, 443));
+            */
+
+            SchemeRegistry registry = new SchemeRegistry();
+            registry.register(new Scheme("https", new TlsSniSocketFactory(), 443));
+            httpClient = new DefaultHttpClient(new ThreadSafeClientConnManager(params, registry),params);
+
+            HttpGet httpPost = new HttpGet(URL);
+            HttpResponse httpResponse = httpClient.execute(httpPost);
+            if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                return EntityUtils.toString(httpResponse.getEntity());
+            } else {
+                return null;
+            }
+        } catch (Exception e) {
+            Log.e("Error", "T" + " " + e.getMessage());
+            httpClient.getConnectionManager().shutdown();
+            return null;
+        }
+    }
+
+}
